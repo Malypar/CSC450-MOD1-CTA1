fa6994aa3137908d154e6dcbacf8851b18747889
Malypar <malykparker55@yahoo.com>
2025-04-20 12:34:59 -0500

    Upload Files
    Adding Source Files

---
+++ CSC450_CT1_mod1-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//MainFunction
+int main() {
+    cout<<"Welcome to this C++ program!"<<endl;
+    cout<<"I have corrected all errors for this program."<<endl;
+
+    // Wait for Output screen
+    //Main Function return Statement
+return 0;
+}
---
+++ FictionalPerson.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::string firstname = "Lamar";
+    std::string lastname = "Jackson";
+    std::string address = "1101 Russell St.";
+    std::string city = "Baltimore";
+    std::string zipcode = "21230";
+
+    std::cout << "First Name: " << firstname << std::endl;
+    std::cout << "Last Name: " << lastname << std::endl;
+    std::cout << "Address: " << address << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zipcode: " << zipcode << std::endl;
+
+    return 0;
+}
